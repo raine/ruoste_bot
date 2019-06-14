84010f979bcac34d4a1c314bf4628138054e04ec
Raine Virta <raine.virta@gmail.com>
2019-06-14 15:36:34 +0300

    disable wipes regularly

--- lib/get-wiped-servers.ts
+++ lib/get-wiped-servers.ts
@@ -5,40 +5,42 @@ import { DateTime } from 'luxon'
 
 const JUST_WIPED_BASE_URL = 'https://just-wiped.net'
 
-const getWipedServersRawPage = () =>
-  got(
+const getWipedServersRawPage = () => {
+  const url =
     JUST_WIPED_BASE_URL +
-      '/rust_servers?' +
-      qs.stringify({
-        country: 'Any',
-        map: 'Any',
-        max_active_player: '500',
-        max_hours_since_wipe: '73',
-        max_max_group: '11',
-        max_players_max: '500',
-        max_wipe_cycle: '31',
-        max_world_size: '6000',
-        min_active_player: '0',
-        min_hours_since_wipe: '0',
-        min_max_group: '1',
-        min_players_max: '24',
-        min_rating: '60',
-        min_wipe_cycle: '0',
-        min_world_size: '1000',
-        mobile_current_player: '0,500',
-        mobile_hours_since_wipe: '0,73',
-        mobile_max_group: '1,11',
-        mobile_max_players: '24,500',
-        mobile_min_rating: '60',
-        mobile_wipe_cycle: '0,31',
-        mobile_world_size: '1000,6000',
-        q: '',
-        region: 'europe',
-        s_type: 'vanilla_only',
-        uptime_badge: '1',
-        wipe_regularity_badge: '1'
-      })
-  ).then((res) => res.body)
+    '/rust_servers?' +
+    qs.stringify({
+      country: 'Any',
+      map: 'Any',
+      max_active_player: '500',
+      max_hours_since_wipe: '73',
+      max_max_group: '11',
+      max_players_max: '500',
+      max_wipe_cycle: '31',
+      max_world_size: '6000',
+      min_active_player: '0',
+      min_hours_since_wipe: '0',
+      min_max_group: '1',
+      min_players_max: '24',
+      min_rating: '60',
+      min_wipe_cycle: '0',
+      min_world_size: '1000',
+      mobile_current_player: '0,500',
+      mobile_hours_since_wipe: '0,73',
+      mobile_max_group: '1,11',
+      mobile_max_players: '24,500',
+      mobile_min_rating: '60',
+      mobile_wipe_cycle: '0,31',
+      mobile_world_size: '1000,6000',
+      q: '',
+      region: 'europe',
+      s_type: 'vanilla_only',
+      uptime_badge: '1',
+      wipe_regularity_badge: '0'
+    })
+
+  return got(url).then((res) => res.body)
+}
 
 export type Server = {
   country: string
@@ -111,6 +113,7 @@ const parseServers = (html: string): Server[] => {
     .get()
 }
 
-const getWipedServers = () => getWipedServersRawPage().then(parseServers)
+const getWipedServers = (): Promise<Server[]> =>
+  getWipedServersRawPage().then(parseServers)
 
 export default getWipedServers
