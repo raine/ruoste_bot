48c3d8d151e154960b0c042cf2b3c184de0c8bc2
Raine Virta <raine.virta@gmail.com>
2021-03-21 19:55:43 +0200

    fixup! move tests to src/test

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,5 @@
 const nockBack = require('nock').back
-nockBack.fixtures = __dirname + '/test/data/'
+nockBack.fixtures = __dirname + '/data/'
 nockBack.setMode('record')
 
 process.env.DATABASE_URL = 'postgres://localhost:5432/ruoste_bot_test'
